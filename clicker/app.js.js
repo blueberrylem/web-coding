d6ff3406b0efc3e7616ac786afe21b82418bdd34
blueberrylem <micahhchi@gmail.com>
2022-07-20 14:53:16 -0700

    my new clicker game

---
+++ clicker/app.js.js
@@ -0,0 +1,28 @@
+
+let button = document.querySelector("button")
+let gameIsRunning = true
+let score=0
+let hp=10
+button.onclick = function(){
+    
+
+    
+    let username= document.querySelector(".a1").value
+
+
+
+
+
+
+    let message = username+", you must win this battle or you will surerly die!"
+    console.log(message)
+    const sendMessageResponseArea = document.querySelector('.response-to-send-message')
+
+}
+  /////  sendMessageResponseArea.textContent = message
+  /// while (gameIsRunning==true) 
+  ///  if (hp==0) {
+   ///    gameIsRunning=false
+    
+
+   
