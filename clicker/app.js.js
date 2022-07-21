6d1d0b705f1f3d3672df1a65c65b7cc7c51aa2cc
blueberrylem <micahhchi@gmail.com>
2022-07-21 09:06:19 -0700

    i might change it

--- clicker/app.js.js
+++ clicker/app.js.js
@@ -4,10 +4,43 @@ let button = document.querySelector("button")
 let gameIsRunning = true
 let score=0
 let hp=10
-document.onkeydown = function(event) {
-    event.key == "down"
-    "jumo.jfif".style["top"] = "200px"
+function respondToUpArrow(event){
+    if(event.key === 'ArrowUp'){
+        console.log("UP")
+        gif.style.top = parseInt(gif.style.top) + 5 + 'px'
+        document.querySelector("gif").style["top"] = "3px"
 }
+}
+///document.onkeydown = function(event) {
+///    console.log("key pressed: " + event.key)
+   /// if (event.key == "ArrowDown")[
+    //gif.style.top = parseInt(gif.style.top) + 5 + 'px';]
+   /// document.querySelector("gif").style["top"] = "3px"
+
+   
+
+/* this goes in your css
+position: fixed;
+left: 0;
+top: 0;
+*/
+
+// let player = document.querySelector("TOOD: put  a selector here that finds your player in the DOM")
+// let position = 0
+// player.style["left"] = position + "px"
+
+// let body = document.querySelector("body")
+// body.addEventListener("keydown", e=>{if(e.key === 'w') console.log("UP")});
+// body.addEventListener("keydown", e=>{if(e.key === 'ArrowUp') console.log("UP")});
+// body.addEventListener("keydown", function(e){if(e.key === 'ArrowUp') console.log("UP")});
+
+//   function respondToUpArrow(event){
+//     if(event.key === 'ArrowUp') {
+//         console.log("UP")
+//     }
+// }
+
+// body.addEventListener("keydown", respondToUpArrow);
 
 button.onclick = function(){
     
