ebaa409b5bd2019b87213a3f173ac46b17cc6c9f
blueberrylem <micahhchi@gmail.com>
2022-07-21 15:09:10 -0700

    newest

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 
-let button = document.querySelector("button")
+let createButtons = document.querySelector(".createButton")
 
 
 
-button.onclick = function(){
+createButtons.onclick = function(){
     
     let ab1 = document.querySelector(".a1").value
     let ab2 = document.querySelector(".a2").value
@@ -27,29 +27,29 @@ button.onclick = function(){
     let ab20 = document.querySelector(".a20").value
     let message = "If you go to some_"+ab1+"_place like Yellowstone National_"+ab2+"_,you must know how to deal with the wild animals such as bears and wolves and_ "+ab3+". The most important of these is the bear. There are three kinds of bears, the grizzly bear, the_"+ab4+"_bear, and the_"+ab5+"_bear.Bears spend most of their time_"+ab6+"_or_"+ab7+". They look very_"+ab8+"_,but if you make them_"+ab9+",_they might bite your_"+ab10+". Bears will come up to your car and beg for_"+ab11+". They will stand on their hind legs and clap their_"+ab12+"_together and pretend to be_"+ab13+". But do not get out of your_"+ab14+"_or offer the bears_"+ab15+"_or_"+ab16+". This same advice applies to other wild creatures such as_"+ab17+"_and_"+ab18+". Remember all these rules and you will spend your vacation_"+ab19+"_and not get eaten by a_"+ab20
     console.log(message)
-    const sendMessageResponseArea = document.querySelector('.response-to-send-message')
+    const madLibsPrintout = document.querySelector('.madLibsPrintout')
     
     
-    sendMessageResponseArea.textContent = message
-    ab1.remove();
-    ab2.remove();
-    ab3.remove();
-    ab4.remove();
-    ab5.remove();
-    ab6.remove();
-    ab7.remove();
-    ab8.remove();
-    ab9.remove();
-    ab10.remove();
-    ab11.remove();
-    ab12.remove();
-    ab13.remove();
-    ab14.remove();
-    ab15.remove();
-    ab16.remove();
-    ab17.remove();
-    ab18.remove();
-    ab19.remove();
-    ab20.remove();
+    madLibsPrintout.textContent = message
+    // ab1.remove();
+    // ab2.remove();
+    // ab3.remove();
+    // ab4.remove();
+    // ab5.remove();
+    // ab6.remove();
+    // ab7.remove();
+    // ab8.remove();
+    // ab9.remove();
+    // ab10.remove();
+    // ab11.remove();
+    // ab12.remove();
+    // ab13.remove();
+    // ab14.remove();
+    // ab15.remove();
+    // ab16.remove();
+    // ab17.remove();
+    // ab18.remove();
+    // ab19.remove();
+    // ab20.remove();
 }
 // hello
