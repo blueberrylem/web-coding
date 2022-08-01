0c9f96704c2bdf6975b038ce69fb55434359ddab
blueberrylem <micahhchi@gmail.com>
2022-08-01 13:50:48 -0700

    fdjfdk

--- app.js
+++ app.js
@@ -1,19 +1,26 @@
 
 let createButtons = document.querySelector(".createButton")
-let submitButton = document.querySelector(".submitButton")
+let ssubmitButton = document.querySelector(".ssubmitButton")
 let startButton = document.querySelector(".startButton")
+let nextButton = document.querySelector(".nextButton")
 let subjectDropdown = document.querySelector("select")
 // const nouns = ["Actor, Airport, Animal, Apple, Army, Balloon, banana, gold, grass, greece, guitar, hair, hamburger, helicopter, helmet, holiday, honey, horse, hospital, painting, parrot, pencil, piano, pillow, pizza, planet, plastic, Portugal, potato, queen, quill, rain, refrigerator, river, iron, insurance, insect, ice, beard, bed, belgium, boy, branch, beach, battery, island, ice, hydrogen, house, hospital, camera, candle, car, caravan, carpet, cartoon, china, church, crayon, crowd, daughter, death, Denmark, library, lighter, leather, lawyer, lamp, knife, kite, kitchen, king, kangaroo, juice, jewellery 
 // "]
 // console.log( valueOfSubject)
 
+nextButton.onclick= function (){
+    document.querySelector(".playerSelector").style.display = "block";
+    document.querySelector(".startScreen").style.display = "none";
+}
 startButton.onclick= function () {
     playAudio()
+    document.querySelector(".playerSelector").style.display = "none";
     console.log("hgf")
     if ( subjectDropdown.value == "builder"){
         console.log("builder")
+        if ( subjectDropdown.value == "1"){
         document.querySelector(".madLibInput").style.display = "block";
-        document.querySelector(".startScreen").style.display = "none";
+        document.querySelector(".startScreen").style.display = "none";}
     }else if ( subjectDropdown.value == "voting"){
         console.log("voter")
         document.querySelector(".startScreen").style.display = "none";
@@ -38,46 +45,45 @@ startButton.onclick= function () {
 
 // selectorPagesTeleporter()
 
-createButtons.onclick = function(){
+ssubmitButton.onclick = function(){
     playAudio()
     document.querySelector(".madLibInput").style.display = "none";
- 
-    let ab1 = document.querySelector(".a1").value
-    let ab2 = document.querySelector(".a2").value
-    let ab3 = document.querySelector(".a3").value
-    let ab4 = document.querySelector(".a4").value
-    let ab5 = document.querySelector(".a5").value
-    let ab6 = document.querySelector(".a6").value
-    let ab7 = document.querySelector(".a7").value
-    let ab8 = document.querySelector(".a8").value
-    let ab9 = document.querySelector(".a9").value
-    let ab10 = document.querySelector(".a10").value
-    let ab11 = document.querySelector(".a11").value
-    let ab12 = document.querySelector(".a12").value
-    let ab13 = document.querySelector(".a13").value
-    let ab14 = document.querySelector(".a14").value
-    let ab15 = document.querySelector(".a15").value
-    let ab16 = document.querySelector(".a16").value
-    let ab17 = document.querySelector(".a17").value
-    let ab18 = document.querySelector(".a18").value
-    let ab19 = document.querySelector(".a19").value
-    let ab20 = document.querySelector(".a20").value
-    let message = "If you go to some_"+ab1+"_place like Yellowstone National_"+ab2+"_,you must know how to deal with the wild animals such as bears and wolves and_ "+ab3+". The most important of these is the bear. There are three kinds of bears, the grizzly bear, the_"+ab4+"_bear, and the_"+ab5+"_bear.Bears spend most of their time_"+ab6+"_or_"+ab7+". They look very_"+ab8+"_,but if you make them_"+ab9+",_they might bite your_"+ab10+". Bears will come up to your car and beg for_"+ab11+". They will stand on their hind legs and clap their_"+ab12+"_together and pretend to be_"+ab13+". But do not get out of your_"+ab14+"_or offer the bears_"+ab15+"_or_"+ab16+". This same advice applies to other wild creatures such as_"+ab17+"_and_"+ab18+". Remember all these rules and you will spend your vacation_"+ab19+"_and not get eaten by a_"+ab20
+    let noun1 = document.querySelector(".noun1").value
+    let noun2 = document.querySelector(".noun2").value
+    let verb1 = document.querySelector(".verb1").value
+    let verb2 = document.querySelector(".verb2").value
+    let adverb1 = document.querySelector(".adverb1").value
+
+    // let ab1 = document.querySelector(".a1").value
+    // let ab2 = document.querySelector(".a2").value
+    // let ab3 = document.querySelector(".a3").value
+    // let ab4 = document.querySelector(".a4").value
+    // let ab5 = document.querySelector(".a5").value
+    // let ab6 = document.querySelector(".a6").value
+    // let ab7 = document.querySelector(".a7").value
+    // let ab8 = document.querySelector(".a8").value
+    // let ab9 = document.querySelector(".a9").value
+    // let ab10 = document.querySelector(".a10").value
+    // let ab11 = document.querySelector(".a11").value
+    // let ab12 = document.querySelector(".a12").value
+    // let ab13 = document.querySelector(".a13").value
+    // let ab14 = document.querySelector(".a14").value
+    // let ab15 = document.querySelector(".a15").value
+    // let ab16 = document.querySelector(".a16").value
+    // let ab17 = document.querySelector(".a17").value
+    // let ab18 = document.querySelector(".a18").value
+    // let ab19 = document.querySelector(".a19").value
+    // let ab20 = document.querySelector(".a20").value
+    // let message = "If you go to some_"+ab1+"_place like Yellowstone National_"+ab2+"_,you must know how to deal with the wild animals such as bears and wolves and_ "+ab3+". The most important of these is the bear. There are three kinds of bears, the grizzly bear, the_"+ab4+"_bear, and the_"+ab5+"_bear.Bears spend most of their time_"+ab6+"_or_"+ab7+". They look very_"+ab8+"_,but if you make them_"+ab9+",_they might bite your_"+ab10+". Bears will come up to your car and beg for_"+ab11+". They will stand on their hind legs and clap their_"+ab12+"_together and pretend to be_"+ab13+". But do not get out of your_"+ab14+"_or offer the bears_"+ab15+"_or_"+ab16+". This same advice applies to other wild creatures such as_"+ab17+"_and_"+ab18+". Remember all these rules and you will spend your vacation_"+ab19+"_and not get eaten by a_"+ab20
+    let message= noun1+" "+ " "+verb1+" "+ " "+adverb1+" "+ " "+noun2+" "+ " "+verb2+"."
     console.log(message)
+
     const madLibsPrintout = document.querySelector('.madLibsPrintout')
     
     
     madLibsPrintout.textContent = message
     document.querySelector(".submitButton").style.display = "block";
 }
-submitButton.onclick = function (){
-    playAudio()
-    console.log("helloWorld") 
-document.querySelector(".submitButton").style.display = "none";
-document.querySelector(".madLibsPrintout").style.display = "none";
-document.querySelector(".trolling").style.display = "block";
-}
-
 
 
 
