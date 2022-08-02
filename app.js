5aecf469a176e1cc4bb61a4dd3c1318b0355f9dc
blueberrylem <micahhchi@gmail.com>
2022-08-02 14:56:29 -0700

    hello?

--- app.js
+++ app.js
@@ -1,172 +1,143 @@
-let noun1 = document.querySelector(".noun1").value
-let noun2 = document.querySelector(".noun2").value
-let verb1 = document.querySelector(".verb1").value
-let verb2 = document.querySelector(".verb2").value
-let adverb1 = document.querySelector(".adverb1").value
-
-let createButtons = document.querySelector(".createButton")
-let startButton = document.querySelector(".startButton")
-let nextButton = document.querySelector(".nextButton")
-let subjectDropdown = document.getElementById("subject")
-let subjectDropdown2 = document.getElementById("subjects")
-let P1Button = document.querySelector(".P1Button")
-let n1SubmitButton=document.querySelector(".n1SubmitButton")
-let n2SubmitButton=document.querySelector(".n2SubmitButton")
-
-let n3SubmitButton=document.querySelector(".n3SubmitButton")
-
-let n4SubmitButton=document.querySelector(".n4SubmitButton")
-
-let n5SubmitButton=document.querySelector(".n5SubmitButton")
+holler.onLoad(()=>{
+    setTimeout(()=> {
+    holler.me(()=>{
 
 
+        let noun1 = document.querySelector(".noun1").value
+        let noun2 = document.querySelector(".noun2").value
+        let verb1 = document.querySelector(".verb1").value  
+        let verb2 = document.querySelector(".verb2").value
+        let adverb1 = document.querySelector(".adverb1").value
 
-const buttons=[createButtons, startButton, nextButton, P1Button]
+        let createButtons = document.querySelector(".createButton")
+        let startButton = document.querySelector(".startButton")
+        let nextButton = document.querySelector(".nextButton")
+        let subjectDropdown = document.getElementById("subject")
+        let subjectDropdown2 = document.getElementById("subjects")
+        let P1Button = document.querySelector(".P1Button")
+        let n1SubmitButton=document.querySelector(".n1SubmitButton")
+        let n2SubmitButton=document.querySelector(".n2SubmitButton")
 
-// let noun = document.getElementById("subjects")
-// let noun1 = document.querySelector(".noun1").value
-// let noun2 = document.querySelector(".noun2").value
-// let verb1 = document.querySelector(".verb1").value
-// let verb2 = document.querySelector(".verb2").value
-// let adverb1 = document.querySelector(".adverb1").value
-// let partsOfSpeech = [noun1, verb1, adverb1, noun2, verb2]
-// Math.floor(Math.random() * partsOfSpeech.length)
-// let partsOfSpeechIndex = Math.floor(Math.random() * partsOfSpeech.length)
-// let counter=1
-// hello=partsOfSpeech[partsOfSpeechIndex]
-// while (counter<6) {
-//     partsOfSpeech[partsOfSpeechIndex]
-//     counter+1
-//     console.log(hello)
-// }
-// const nouns = ["Actor, Airport, Animal, Apple, Army, Balloon, banana, gold, grass, greece, guitar, hair, hamburger, helicopter, helmet, holiday, honey, horse, hospital, painting, parrot, pencil, piano, pillow, pizza, planet, plastic, Portugal, potato, queen, quill, rain, refrigerator, river, iron, insurance, insect, ice, beard, bed, belgium, boy, branch, beach, battery, island, ice, hydrogen, house, hospital, camera, candle, car, caravan, carpet, cartoon, china, church, crayon, crowd, daughter, death, Denmark, library, lighter, leather, lawyer, lamp, knife, kite, kitchen, king, kangaroo, juice, jewellery 
-// "]
-// console.log( valueOfSubject)
+        let n3SubmitButton=document.querySelector(".n3SubmitButton")
+
+        let n4SubmitButton=document.querySelector(".n4SubmitButton")
+
+        let n5SubmitButton=document.querySelector(".n5SubmitButton")
 
 
-nextButton.onclick= function (){
-    document.querySelector(".player").style.display = "block";
-    document.querySelector(".startScreen").style.display = "none"; 
-}
-startButton.onclick= function () {
-    playAudio()
-    document.querySelector(".player").style.display = "none";
-    console.log("hgf")
-    if ( subjectDropdown.value == "builder"){
-        console.log("builder")
-        if ( subjectDropdown2.value == "1"){
-            console.log("you did it")
-            document.querySelector(".nounInput").style.display = "block";
-            
-            document.querySelector(".startScreen").style.display = "none";
+
+        const buttons=[createButtons, startButton, nextButton, P1Button]
+
+    // let noun = document.getElementById("subjects")
+    // let noun1 = document.querySelector(".noun1").value
+    // let noun2 = document.querySelector(".noun2").value
+    // let verb1 = document.querySelector(".verb1").value
+    // let verb2 = document.querySelector(".verb2").value
+    // let adverb1 = document.querySelector(".adverb1").value
+    // let partsOfSpeech = [noun1, verb1, adverb1, noun2, verb2]
+    // Math.floor(Math.random() * partsOfSpeech.length)
+    // let partsOfSpeechIndex = Math.floor(Math.random() * partsOfSpeech.length)
+    // let counter=1
+    // hello=partsOfSpeech[partsOfSpeechIndex]
+    // while (counter<6) {
+    //     partsOfSpeech[partsOfSpeechIndex]
+    //     counter+1
+    //     console.log(hello)
+    // }
+    // const nouns = ["Actor, Airport, Animal, Apple, Army, Balloon, banana, gold, grass, greece, guitar, hair, hamburger, helicopter, helmet, holiday, honey, horse, hospital, painting, parrot, pencil, piano, pillow, pizza, planet, plastic, Portugal, potato, queen, quill, rain, refrigerator, river, iron, insurance, insect, ice, beard, bed, belgium, boy, branch, beach, battery, island, ice, hydrogen, house, hospital, camera, candle, car, caravan, carpet, cartoon, china, church, crayon, crowd, daughter, death, Denmark, library, lighter, leather, lawyer, lamp, knife, kite, kitchen, king, kangaroo, juice, jewellery 
+    // "]
+    // console.log( valueOfSubject)
+
+
+        nextButton.onclick= function (){
+            document.querySelector(".player").style.display = "block";
+            document.querySelector(".startScreen").style.display = "none"; 
         }
-        if ( subjectDropdown2.value == "2"){
-            console.log("you did it")
-            document.querySelector(".nounInput2").style.display = "block";
-            document.querySelector(".startScreen").style.display = "none";
-        }
-        if ( subjectDropdown2.value == "3"){
-            console.log("you did it")
-            document.querySelector(".nounInput3").style.display = "block";
-            document.querySelector(".startScreen").style.display = "none";
-        }
-        if ( subjectDropdown2.value == "4"){
-            console.log("you did it")
-            document.querySelector(".nounInput4").style.display = "block";
-            document.querySelector(".startScreen").style.display = "none";
-        }
-        if ( subjectDropdown2.value == "5"){
-            console.log("you did it")
-            document.querySelector(".nounInput5").style.display = "block";
-            document.querySelector(".startScreen").style.display = "none";
-        }
-    }else if ( subjectDropdown.value == "voting"){
-        console.log("voter")
-        document.querySelector(".startScreen").style.display = "none";
-        document.querySelector(".loading").style.display = "block";
-        let startmessage="Press any key To Start"
-        const pressSStart  = document.querySelector('.pressSStart')
-        pressSStart.textContent=startmessage
-        document.onkeydown=function () {
+        startButton.onclick= function () {
             playAudio()
-            console.log("dkfjadkfjdlakfjadslfkja;sdlkfj;dlaskjfldkjas;f");
-            let startmessage=""
-            const pressSStart  = document.querySelector('.pressSStart')
-            pressSStart.textContent=startmessage
-            document.querySelector(".loading").style.display = "none";
-            document.querySelector(".votingPage").style.display = "block";
-            document.querySelector(".p1vote").style.display = "block";
-            document.querySelector(".p2vote").style.display = "block";
-            document.querySelector(".p3vote").style.display = "block";
-            document.querySelector(".p4vote").style.display = "block";
-            document.querySelector(".p5vote").style.display = "block";
+            document.querySelector(".player").style.display = "none";
+            console.log("hgf")
+            if ( subjectDropdown.value == "builder"){
+                console.log("builder")
+                if ( subjectDropdown2.value == "1"){
+                    console.log("you did it")
+                    document.querySelector(".nounInput").style.display = "block";
+                    
+                    document.querySelector(".startScreen").style.display = "none";
+                }
+                if ( subjectDropdown2.value == "2"){
+                    console.log("you did it")
+                    document.querySelector(".nounInput2").style.display = "block";
+                    document.querySelector(".startScreen").style.display = "none";
+                }
+                if ( subjectDropdown2.value == "3"){
+                    console.log("you did it")
+                    document.querySelector(".nounInput3").style.display = "block";
+                    document.querySelector(".startScreen").style.display = "none";
+                }
+                if ( subjectDropdown2.value == "4"){
+                    console.log("you did it")
+                    document.querySelector(".nounInput4").style.display = "block";
+                    document.querySelector(".startScreen").style.display = "none";
+                }
+                if ( subjectDropdown2.value == "5"){
+                    console.log("you did it")
+                    document.querySelector(".nounInput5").style.display = "block";
+                    document.querySelector(".startScreen").style.display = "none";
+                }
+            }else if ( subjectDropdown.value == "voting"){
+                console.log("voter")
+                document.querySelector(".startScreen").style.display = "none";
+                document.querySelector(".loading").style.display = "block";
+                let startmessage="Press any key To Start"
+                const pressSStart  = document.querySelector('.pressSStart')
+                pressSStart.textContent=startmessage
+                document.onkeydown=function () {
+                    playAudio()
+                    console.log("dkfjadkfjdlakfjadslfkja;sdlkfj;dlaskjfldkjas;f");
+                    let startmessage=""
+                    const pressSStart  = document.querySelector('.pressSStart')
+                    pressSStart.textContent=startmessage
+                    document.querySelector(".loading").style.display = "none";
+                    document.querySelector(".votingPage").style.display = "block";
+                    document.querySelector(".p1vote").style.display = "block";
+                    document.querySelector(".p2vote").style.display = "block";
+                    document.querySelector(".p3vote").style.display = "block";
+                    document.querySelector(".p4vote").style.display = "block";
+                    document.querySelector(".p5vote").style.display = "block";
+                    
+
+                }
+            }
             
-
         }
-    //     }
-    
-}
-
-// selectorPagesTeleporter()
-}
-n1SubmitButton.onclick = function(){
-    let noun1 = document.querySelector(".noun1").value
-    // const collection=[noun1]
-    playAudio()
-   
-    let message= "hello"+noun1+" "+ " "+verb1+" "+ " "+adverb1+" "+ " "+noun2+" "+ " "+verb2+""
-    console.log(message)
-    
-
-n2SubmitButton.onclick = function(){
-    playAudio()
-    let verb1 = document.querySelector(".verb1").value
-    const madLibsPrintout = document.querySelector('.madLibsPrintout')
-    let message= "hello"+noun1+" "+ " "+verb1+" "+ " "+adverb1+" "+ " "+noun2+" "+ " "+verb2+""
-    console.log(message)
-    madLibsPrintout.textContent = message
-
-P1Button.onclick = function(){
-    document.querySelector(".giphyy").style.display = "block";
-}
-
-
-
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  
-}
+        holler.onClientEvent(event=>{
+            console.log(`Client event received: ${event}`)
+        })
+        // selectorPagesTeleporter()
+        // }
+        n1SubmitButton.onclick = function(){
+            let noun1 = document.querySelector(".noun1").value
+            // const collection=[noun1]
+            playAudio()
+        
+            holler.appInstance.notifyClients("hi")
+        }
+        n2SubmitButton.onclick = function(){
+            playAudio()
+            let verb1 = document.querySelector(".verb1").value
+            const madLibsPrintout = document.querySelector('.madLibsPrintout')
+            const message= ["hello"+noun1+" "+ " "+verb1+" "+ " "+adverb1+" "+ " "+noun2+" "+ " "+verb2+"."]
+            console.log(message)
+            madLibsPrintout.textContent = message
+            holler.appInstance.notifyClients(verb1)
+        }
+        P1Button.onclick = function(){
+            document.querySelector(".giphyy").style.display = "block";
+        }
+    }, 1000)
+    })
+})
   // ab1.remove();
     // ab2.remove();
     // ab3.remove();
@@ -206,9 +177,9 @@ P1Button.onclick = function(){
     // let ab14 = document.querySelector(".a14").value
     // let ab15 = document.querySelector(".a15").value
     // let ab16 = document.querySelector(".a16").value
-    // let ab17 = document.querySelector(".a17").value
+    // let ab17 = document.querySelector(".a17").valu
     // let ab18 = document.querySelector(".a18").value
     // let ab19 = document.querySelector(".a19").value
-    // let ab20 = document.querySelector(".a20").value
+    // let ab20 = document.querySelector(".a20").value}
     
     // let message = "If you go to some_"+ab1+"_place like Yellowstone National_"+ab2+"_,you must know how to deal with the wild animals such as bears and wolves and_ "+ab3+". The most important of these is the bear. There are three kinds of bears, the grizzly bear, the_"+ab4+"_bear, and the_"+ab5+"_bear.Bears spend most of their time_"+ab6+"_or_"+ab7+". They look very_"+ab8+"_,but if you make them_"+ab9+",_they might bite your_"+ab10+". Bears will come up to your car and beg for_"+ab11+". They will stand on their hind legs and clap their_"+ab12+"_together and pretend to be_"+ab13+". But do not get out of your_"+ab14+"_or offer the bears_"+ab15+"_or_"+ab16+". This same advice applies to other wild creatures such as_"+ab17+"_and_"+ab18+". Remember all these rules and you will spend your vacation_"+ab19+"_and not get eaten by a_"+ab20
